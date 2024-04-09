225e1ca7c9c2e99606e66ff62a14bcd04b43d10b
laww10 <114757219+laww10@users.noreply.github.com>
2024-04-09 23:12:20 +0000

    done

--- app.rb
+++ app.rb
@@ -1,9 +1,67 @@
 require "sinatra"
 require "sinatra/reloader"
 
+moves = ["rock","paper","scissors"]
+
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home, {:layout => :wrapper})
+
+end
+
+get("/rock") do
+  
+  computer_move = moves.sample
+
+  if computer_move == "paper"
+    success = "lost"
+  elsif computer_move == "rock"
+    success = "tied"
+  else
+    success = "won"
+  end
+
+  @move = "They played #{computer_move}!"
+  @outcome = "We #{success} !"
+
+  erb(:rock,{:layout => :wrapper} )
+
+end
+
+get("/paper") do
+  
+  computer_move = moves.sample
+
+  if computer_move == "paper"
+    success = "tied"
+  elsif computer_move == "rock"
+    success = "won"
+  else
+    success = "lost"
+  end
+
+  @move = "They played #{computer_move}!"
+  @outcome = "We #{success} !"
+
+  erb(:paper,{:layout => :wrapper} )
+
+end
+
+get("/scissors") do
+  
+  computer_move = moves.sample
+
+  if computer_move == "scissors"
+    success = "tied"
+  elsif computer_move == "rock"
+    success = "lost"
+  else
+    success = "won"
+  end
+
+  @move = "They played #{computer_move}!"
+  @outcome = "We #{success} !"
+
+  erb(:scissors,{:layout => :wrapper} )
+
 end
